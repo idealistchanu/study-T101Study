a875b65a3c3865c671499a1eeb0afcf6e2bcd3ae
정찬우 <idealistchanu@gmail.com>
2022-11-19 17:42:52 +0900

    for_each, if/else Sub Mission

---
+++ CHAPTER5/MISSION/main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a77be80595e6ded5ecff4326bb71223015d42dc7186502a5e49542777d68033c
+size 535
---
+++ CHAPTER5/MISSION/variables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d7c6bd7fab6c2696d7fbba6bf2f36f4313871fac543d6ea9eef2c69c8497668
+size 339
